30c0b24b6460d9be779b337d7aa7ac2d66263846
arielcessario <arielcessario@gmail.com>
2016-08-06 14:02:15 -0300

    a

---
+++ app/app.routes.ts
@@ -0,0 +1,28 @@
+import { provideRouter, RouterConfig }  from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+export const routes: RouterConfig = [
+    {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+    },
+    {
+        path: 'heroes',
+        component: HeroesComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    }
+];
+
+export const appRouterProviders = [
+    provideRouter(routes)
+];
---
+++ app/rxjs-operators.js
@@ -0,0 +1,7 @@
+BAD;
+FILENAME: .. / .. / .. / _fragments / toh - 6 / ts / app / rxjs - operators.ts.md;
+Current;
+path: docs, ts, latest, tutorial, toh - pt6;
+PathToDocs: .. / .. / .. /
+;
+//# sourceMappingURL=rxjs-operators.js.map
